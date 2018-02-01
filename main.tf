a6889c4220b0722db95250e382a33178ac67d623
leszekg <gonczar@gmail.com>
2018-02-01 10:55:04 +0000

    Prepare the module to create an Azure Service Bus queue

---
+++ main.tf
@@ -0,0 +1,21 @@
+# ARM template for Service Bus queue
+data "template_file" "queue_template" {
+  template = "${file("${path.module}/template/queue_template.json")}"
+}
+
+# Create Azure Service Bus queue
+resource "azurerm_template_deployment" "queue" {
+  template_body       = "${data.template_file.queue_template.rendered}"
+  name                = "${var.name}"
+  deployment_mode     = "Incremental"
+  resource_group_name = "${var.resource_group_name}"
+
+  parameters = {
+    serviceBusNamespaceName             = "${var.namespace_name}"
+    serviceBusQueueName                 = "${var.name}"
+    lockDuration                        = "${var.lock_duration}"
+    maxDeliveryCount                    = "${var.max_delivery_count}"
+    requiresDuplicateDetection          = "${var.requires_duplicate_detection}"
+    duplicateDetectionHistoryTimeWindow = "${var.duplicate_detection_history_time_window}"
+  }
+}
---
+++ output.tf
@@ -0,0 +1,39 @@
+# primary connection string for send operations
+output "primary_send_connection_string" {
+  value = "${azurerm_template_deployment.queue.outputs["primarySendConnectionString"]}"
+}
+
+# secondary connection string for send operations
+output "secondary_send_connection_string" {
+  value = "${azurerm_template_deployment.queue.outputs["secondarySendConnectionString"]}"
+}
+
+# primary shared access key with send rights
+output "primary_send_shared_access_key" {
+  value = "${azurerm_template_deployment.queue.outputs["primarySendSharedAccessKey"]}"
+}
+
+# secondary shared access key with send rights
+output "secondary_send_shared_access_key" {
+  value = "${azurerm_template_deployment.queue.outputs["secondarySendSharedAccessKey"]}"
+}
+
+# primary connection string for listen operations
+output "primary_listen_connection_string" {
+  value = "${azurerm_template_deployment.queue.outputs["primaryListenConnectionString"]}"
+}
+
+# secondary connection string for listen operations
+output "secondary_listen_connection_string" {
+  value = "${azurerm_template_deployment.queue.outputs["secondaryListenConnectionString"]}"
+}
+
+# primary shared access key with listen rights
+output "primary_listen_shared_access_key" {
+  value = "${azurerm_template_deployment.queue.outputs["primaryListenSharedAccessKey"]}"
+}
+
+# secondary shared access key with listen rights
+output "secondary_listen_shared_access_key" {
+  value = "${azurerm_template_deployment.queue.outputs["secondaryListenSharedAccessKey"]}"
+}
